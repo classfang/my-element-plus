39a0e6fe8a15fbe2475dd14c8cf2301192234d53
jjfang7 <jjfang7@iflytek.com>
2023-10-24 19:09:34 +0800

    Initial commit

---
+++ es/components/card/src/card.mjs
@@ -0,0 +1,42 @@
+import { createElementVNode, defineComponent, openBlock, createElementBlock, normalizeClass, unref, renderSlot, createTextVNode, toDisplayString, createCommentVNode, normalizeStyle } from 'vue';
+import '../../../hooks/index.mjs';
+import { cardProps } from './card2.mjs';
+import _export_sfc from '../../../_virtual/plugin-vue_export-helper.mjs';
+import { useNamespace } from '../../../hooks/use-namespace/index.mjs';
+
+const _hoisted_1 = /* @__PURE__ */ createElementVNode("div", null, "\u8BE5\u7EC4\u4EF6\u88AB\u4FEE\u6539", -1);
+const __default__ = defineComponent({
+  name: "ElCard"
+});
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  ...__default__,
+  props: cardProps,
+  setup(__props) {
+    const ns = useNamespace("card");
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", {
+        class: normalizeClass([unref(ns).b(), unref(ns).is(`${_ctx.shadow}-shadow`)])
+      }, [
+        _hoisted_1,
+        _ctx.$slots.header || _ctx.header ? (openBlock(), createElementBlock("div", {
+          key: 0,
+          class: normalizeClass(unref(ns).e("header"))
+        }, [
+          renderSlot(_ctx.$slots, "header", {}, () => [
+            createTextVNode(toDisplayString(_ctx.header), 1)
+          ])
+        ], 2)) : createCommentVNode("v-if", true),
+        createElementVNode("div", {
+          class: normalizeClass([unref(ns).e("body"), _ctx.bodyClass]),
+          style: normalizeStyle(_ctx.bodyStyle)
+        }, [
+          renderSlot(_ctx.$slots, "default")
+        ], 6)
+      ], 2);
+    };
+  }
+});
+var Card = /* @__PURE__ */ _export_sfc(_sfc_main, [["__file", "/Users/junki/projects/element-plus/packages/components/card/src/card.vue"]]);
+
+export { Card as default };
+//# sourceMappingURL=card.mjs.map
